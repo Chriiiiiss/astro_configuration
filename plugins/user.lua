c9febf34f31becc542b89b37a40108c32b81eacf
Chris <chris.fraanc@gmail.com>
2023-12-16 17:18:58 +0100

    ADD todo-comments plugin, removing useless comments

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,10 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim"},
+    opts = {
+      signs = true,
+    },
+    event = "User Astrofile",
+  }
 }
